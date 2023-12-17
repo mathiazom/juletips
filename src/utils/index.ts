d14fe7e597d38e04719d79fcf72216158aac8e0c
Mathias Oterhals Myklebust <mathias@oterbust.no>
2023-12-17 12:22:23 +0100

    chore: move current year function to .ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,3 @@
-export function formatDate(date: string) {
-    return new Date(date).toLocaleDateString("en-US", {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-    });
+export function getCurrentYear() {
+    return new Date().getFullYear();
 }
